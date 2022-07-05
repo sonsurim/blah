e2391fae9d0af28626a503c6d831b69d559b6b69
sonsurim <surim014@naver.com>
2022-07-06 02:09:45 +0900

    feat: 질문 input 컴포넌트 구현

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
+import ResizeTextArea from 'react-textarea-autosize';
 import { ServiceLayout } from '@/components/service_layout';
 
 const userInfo = {
@@ -11,6 +12,24 @@ const userInfo = {
 };
 
 const UserHomePage: NextPage = function () {
+  const [message, setMessage] = useState('');
+  const toast = useToast();
+
+  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    const lineCount = e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 1;
+
+    if (lineCount + 1 > 7) {
+      toast({
+        status: 'error',
+        title: '최대 7줄까지만 입력이 가능합니다!',
+        position: 'top-right',
+      });
+      return;
+    }
+
+    setMessage(e.currentTarget.value);
+  };
+
   return (
     <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
@@ -21,6 +40,28 @@ const UserHomePage: NextPage = function () {
               <Text fontSize="md">{userInfo.displayName}</Text>
               <Text fontSize="xs">{userInfo.email}</Text>
             </Flex>
+          </Flex>
+        </Box>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+          <Flex align="center" p="2">
+            <Avatar size="xs" src="https://bit.ly/broken-link" mr="2" />
+            <Textarea
+              bg="gray.100"
+              border="none"
+              placeholder="무엇이 궁금하신가요?"
+              resize="none"
+              minH="unset"
+              overflow="hidden"
+              fontSize="xs"
+              mr="2"
+              maxRows={7}
+              as={ResizeTextArea}
+              value={message}
+              onChange={(e) => handleChange(e)}
+            />
+            <Button disabled={!message} bgColor="#FFBB86C" color="white" colorScheme="yellow" variant="solid" size="sm">
+              등록
+            </Button>
           </Flex>
         </Box>
       </Box>
